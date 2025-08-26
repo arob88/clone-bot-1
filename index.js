b8ac4af5715e983e6e77b982f88746e384548c04
arob88 <andrewrobinette888@gmail.com>
2025-08-25 22:48:58 -0400

    Update index.js
    Added MissionImpossible.mp3 and Crazy.mp3 webhook routes

--- index.js
+++ index.js
@@ -22,6 +22,32 @@ app.post('/webhook', (req, res) => {
     res.send(twiml);
 });
 
+app.post('/webhook-mission', (req, res) => {
+    // This is the TwiML response that tells Twilio to play your intro MP3
+    const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Play>https://clone-bot-1-pqk0.onrender.com/MissionImpossible.mp3</Play>
+    <Pause length="1"/>
+    <Say voice="alice">Thank you for listening to my intro. Have a great day!</Say>
+</Response>`;
+    
+    res.type('text/xml');
+    res.send(twiml);
+});
+
+app.post('/webhook-crazy', (req, res) => {
+    // This is the TwiML response that tells Twilio to play your intro MP3
+    const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Play>https://clone-bot-1-pqk0.onrender.com/Crazy.mp3</Play>
+    <Pause length="1"/>
+    <Say voice="alice">Thank you for listening to my intro. Have a great day!</Say>
+</Response>`;
+    
+    res.type('text/xml');
+    res.send(twiml);
+});
+
 // Health check endpoint
 app.get('/', (req, res) => {
     res.send('Voice Clone Webhook is running!');
